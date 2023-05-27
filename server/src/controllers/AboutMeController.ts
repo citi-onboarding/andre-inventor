4b464381150871fdefcc3cbde25727151bc78400
Túlio <tulio.lira@outlook.com>
2023-05-27 11:52:27 -0300

    Terminei o controller e adicionei as rotas

--- server/src/controllers/AboutMeController.ts
+++ server/src/controllers/AboutMeController.ts
@@ -1,6 +1,7 @@
 import { Citi, Crud } from "src/global"
 import { Request, Response } from "express"
 import { AboutMe } from "@models/AboutMe"
+import { isatty } from "tty"
 
 export default class AboutMeController implements Crud{
 //Create
@@ -16,10 +17,12 @@ export default class AboutMeController implements Crud{
 
         return res.status(httpStatus).send({ message })
     }
-    async read(req:Request, res:Response){
+
+    async get(req:Request, res:Response){
         const {httpStatus, values: AboutMes} = await Citi.getAll(AboutMe)
         return res.status(httpStatus).send({ AboutMes })
     }
+
     async delete(req:Request, res:Response){
         const { id } = req.params
         const{ value:AboutMeFound, message } = await Citi.findByID(AboutMe, id)
@@ -28,5 +31,19 @@ export default class AboutMeController implements Crud{
         
         const { httpStatus, messageFromDelete } = await Citi.deleteValue(AboutMe, AboutMeFound)
         return res.status(httpStatus).send({ messageFromDelete })
+    }
+
+    async update(req:Request, res:Response){
+        const { id } = req.params
+        const { title, description, image } = req.body
+
+        const isAnyUndefined = Citi.areValuesUndefined(title,description,image)
+
+        if(isAnyUndefined) return res.status(400).send()
+
+        const AboutMeAtualizada = { title, description, image }
+        const{httpStatus, messageFromUpdate} = await Citi.updateValue(AboutMe,id,AboutMeAtualizada)
+
+        return res.status(httpStatus).send({ messageFromUpdate })
     }
 }
