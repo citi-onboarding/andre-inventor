eb7576fca7fa8e5a9b6b5e42dec385a93476af44
Túlio <tulio.lira@outlook.com>
2023-05-28 08:50:06 -0300

    Project (Controller + Model + routes)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 import UserController from '@controllers/UserController'
+import ProjectController from '@controllers/ProjetoController'
 
 const routes = express.Router();
 const userController = new UserController();
+const projectController = new ProjectController();
 
 routes.post('/user', userController.create);
 routes.get('/user', userController.get);
 routes.delete('/user/:id', userController.delete);
 routes.put('/user/:id', userController.update);
 
+routes.post('/projects',projectController.create);
+routes.get('/projects', projectController.get);
+routes.delete('/projects/:id', projectController.delete);
+routes.put('/projects/:id', projectController.update);
 
 export default routes;
