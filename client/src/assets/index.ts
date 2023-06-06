43aeb185fa47d3b100f1f076d801ed4c6cdc2bdb
Túlio <tulio.lira@outlook.com>
2023-06-06 16:10:39 -0300

    Home visual com desenhos (sem conectar com o back)

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,5 +1,19 @@
-import LogoCITi from './logo.png';
+import lightbulb from './lightbulb.png';
+import magic_wand from './magic_wand.png';
+import megaphone from './megaphone.png';
+import rocket from './rocket.png';
+import swirl from './swirl.png';
+import swirlLeft from './swirlLeft.png';
+import trophy from './trophy.png';
+import thought from './thought_bubble.png';
 
 export {
-    LogoCITi
+    lightbulb,
+    magic_wand,
+    megaphone,
+    rocket,
+    swirl,
+    swirlLeft,
+    trophy,
+    thought
 };
--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,24 +1,30 @@
 import React from "react";
 import { HomeContainer } from './styles';
-import { LogoCITi } from '../../assets';
-
+import { lightbulb } from '../../assets';
+import { trophy } from '../../assets';
+import { thought } from '../../assets';
+import { swirl } from '../../assets';
+import { swirlLeft } from '../../assets';
+import { magic_wand } from '../../assets';
+import { megaphone } from '../../assets';
+import { rocket } from '../../assets';
 
 export const Home: React.FC = () => {
     return (
         <HomeContainer>
-            <img src={LogoCITi} alt="" />
-            <h1>ReactJS Boilerplate</h1>
-            <p>
-                Made with
-                {' '}
-                <strong>&lt; &#x0002F; &gt;</strong>
-                {' '}
-                and
-                {' '}
-                <strong>&hearts;</strong>
-                {' '}
-                by CITi
-            </p>
+            <h1 className = "titulo">
+                Conectando pessoas através da inteligência criativa
+            </h1>
+            <p>Podemos ser <b>cocriadores</b> das nossas ideias</p>
+            <p>Vem pra cá!</p>
+            <img className = "light" src = { lightbulb }/>
+            <img className = "trophy" src = { trophy }/>
+            <img className = "thought" src = { thought }/>
+            <img className = "swirl" src = { swirl }/>
+            <img className = "swirlLeft" src = { swirlLeft }/>
+            <img className = "magic_wand" src = { magic_wand }/>
+            <img className = "megaphone" src = { megaphone }/>
+            <img className = "rocket" src = { rocket }/>
         </HomeContainer>
     );
 }
