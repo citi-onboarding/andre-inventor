d512080efce5ae87386405d70a76474a2d2d452e
Túlio <tulio.lira@outlook.com>
2023-06-09 19:23:43 -0300

    Palavras em negrito + botão referenciando site

--- client/src/pages/e-book/index.tsx
+++ client/src/pages/e-book/index.tsx
@@ -7,9 +7,9 @@ export const EBook: React.FC = () => {
             <img src= { Phone } alt="phone_with_ebook" />
             <h1>E POR QUE NÃO?</h1>
             <h2>Desbloqueie sua criatividade!</h2>
-            <p>Um E-book que contém “fórmulas” para despertar e estimular a CRIATIVIDADE e fazer com que você tenha IDEIAS INCRÍVEIS para melhorar a sua vida e também a de outras pessoas.<br/><br/> Resolvendo problemas com uma IDEIA de grande alcance! Você pode até se tornar uma pessoa milionária, feliz e realizada.<br/><br/> Encontre a sua “fórmula”, viva a sua história. <br/>
-            VEM PRA CÁ!</p>
-            <a href="#" className = "btn b1">Comprar E-book</a>
+            <p>Um E-book que contém “fórmulas” para despertar e estimular a <b>CRIATIVIDADE</b> e fazer com que você tenha IDEIAS <b>INCRÍVEIS</b> para melhorar a sua vida e também a de outras pessoas.<br/><br/> Resolvendo problemas com uma <b>IDEIA</b> de grande alcance! Você pode até se tornar uma pessoa milionária, feliz e realizada.<br/><br/> Encontre a sua “fórmula”, viva a sua história. <br/><br/>
+            <b>VEM PRA CÁ!</b></p>
+            <a target="_blank" href="https://hotmart.com/pt-br/marketplace/produtos/e-por-que-nao/U79727696B" className = "btn b1">Comprar E-book</a>
         </EBookContainer>
     );
 }
