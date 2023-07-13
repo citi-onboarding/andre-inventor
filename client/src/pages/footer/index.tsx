bc48e786e9b58e2013cbd00406d61ac5b888ffd6
pedrosantiago20 <pedrouser19@gmail.com>
2023-07-13 18:43:22 -0300

    correcting code

--- client/src/pages/footer/index.tsx
+++ client/src/pages/footer/index.tsx
@@ -1,31 +1,33 @@
 import React from "react";
-import { FooterContainer } from "./style";
+import { FooterContainer, Branco, Content, Div, Img } from "./style";
 import { instagram, facebook, LogoBranco, citi } from '../../assets';
 
 export const Footer: React.FC = () => {
     return (
         
         <FooterContainer>
-            <div className = "preto">
-                <p>Meu objetivo é conectar pessoas através da imaginação criativa.</p>
-                <img src={ LogoBranco } alt="white_logo" className="LogoBranco"/>
-                <a target="_blank" href = "https://www.instagram.com/andreinventor/"><img src={ instagram } alt="instagram_logo" className="instagram"/></a>
-                <a target="_blank" href = 'https://www.facebook.com/andre.freire.3551/'><img src={ facebook } alt="facebook_logo" className="facebook"/></a>
-            </div>
-            <div className="branco">
-            <p className="pciti">
-                Made with
-                {' '}
-                <strong>&lt; &#x0002F; &gt;</strong>
-                {' '}
-                and
-                {' '}
-                <strong>&hearts;</strong>
-                {' '}
-                by 
-            </p>
-            <img src={ citi } alt="logo_citi" className="citi"/>
-            </div>
+            <Content>
+                <Div>
+                <img src={ LogoBranco } alt="white_logo"/>
+                <p style={{maxWidth: '275px', marginLeft: '35px'}}>Meu objetivo é conectar pessoas através da imaginação criativa.</p>
+                </Div>
+                <Img>
+                <a target="_blank" href = "https://www.instagram.com/andreinventor/" rel="noreferrer"><img src={ instagram } alt="instagram_logo"/></a>
+                <a target="_blank" href = 'https://www.facebook.com/andre.freire.3551/' rel="noreferrer"><img src={ facebook } alt="facebook_logo"/></a>
+                </Img>
+            </Content>
+            <Branco>
+                    Made with
+                    {' '}
+                    <strong >&lt; &#x0002F; &gt;</strong>
+                    {' '}
+                    and
+                    {' '}
+                    <strong>&hearts;</strong>
+                    {' '}
+                    by 
+                <img style={{marginLeft: '10px'}} src={ citi } alt="logo_citi"/>
+            </Branco>
         </FooterContainer>
     );
 }
--- client/src/pages/footer/style.ts
+++ client/src/pages/footer/style.ts
@@ -3,94 +3,56 @@ import styled from 'styled-components';
 
 export const FooterContainer = styled.div`
     position: absolute;
-    width: 1366px;
+    width: 100%;
     height: 427px;
-    left: 0px;
-    top: 4101px;
-    .preto{
-        position: absolute;
-        left: 0%;
-        right: 0%;
-        top: 0%;
-        bottom: 14.52%;
-
-        background: #000000;
-    }
-    .branco{
-        position: absolute;
-        left: 0%;
-        right: 0%;
-        top: 0%;
-        bottom: 14.52%;
-
-        background: #000000;
-    }
-    .LogoBranco{
-        position: absolute;
-        height: 87.0102767944336px;
-        width: 178px;
-        left: 100px;
-        top: 176px;
-        border-radius: 0px;
-        z-index: 1;
-    }
-    .instagram{
-        position: absolute;
-        height: 48px;
-        width: 48px;
-        left: 1122px;
-        top: 192px;
-        border-radius: 0px;
-        z-index: 1;
-    }
-    .facebook{
-        position: absolute;
-        height: 48px;
-        width: 48px;
-        left: 1218px;
-        top: 190px;
-        border-radius: 0px;
-        z-index: 1;
-    }
-    p{
-        position: absolute;
-        font-family: 'Sora';
-        font-size: 14px;
-        font-weight: 400;
-        line-height: 18px;
-        letter-spacing: 0em;
-        text-align: left;
-        height: 36px;
-        width: 275px;
-        left: 340px;
-        top: 190px;
-        border-radius: nullpx;
-        z-index: 1;
-        color: white;
-    }
-    .pciti{
-        position: absolute;
-        height: 18.216703414916992px;
-        width: 248px;
-        left: 994px;
-        top: 387px;
-        border-radius: nullpx;
-
-        font-family: 'Sora';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 18px;
-        line-height: 21px;
-        text-align: center;
-        
-        color: #000000;
-    }
-    .citi{
-        position: absolute;
-        height: 18.293935775756836px;
-        width: 31.3271484375px;
-        left: 1234.6728515625px;
-        top: 387.021484375px;
-        border-radius: 0px;
-    }
+    bottom: 0;
+    background-color: black;
 `;
+
+export const Branco = styled.div`
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    background-color: white;
+    height: 62px;
+    text-align: right;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    padding-right: 5%;
+
+    color: black;
+    
+`;
+
+export const Content = styled.div`
+    padding-top: 194px;
+    display: flex;
+    flex-direction: row;
+    padding-left: 5%;
+    padding-right: 5%;
+    justify-content: space-between;
+    
+
+`;
+
+export const Div = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    font-family: Sora;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+
+`;
+
+export const Img = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 145px;
+
+`;
+
